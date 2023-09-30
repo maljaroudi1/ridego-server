ebd331edcbfa6f9f19985ea603242156a827074b
maljaroudi1 <meojax.ajx@gmail.com>
2023-09-29 21:46:58 -0400

    new files

--- api/loginregister.js
+++ api/loginregister.js
@@ -15,13 +15,19 @@ app.use(bodyParser);
 const payload = {
     userId: 1
 };
+
+
 const theSecretKey = process.env.SECRET_KEY;
+
+
+
+const token = jwt.sign(payload, theSecretKey, { expiresIn: '1h' });
 
 // Always declare cors above all get and post requests
 app.use(cors({
-    origin: 'https://car-rental-rentgo.vercel.app',
-    allowedHeaders: 'Content-Type, Authorization',
-    methods: 'GET, POST', // Specify allowed HTTP methods
+  origin: 'https://car-rental-rentgo.vercel.app',
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify the allowed headers as an array
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed methods as an array
 }));
 
 // UserScheme for customer-info
@@ -64,55 +70,85 @@ UserSchema.pre('save', async function (next) {
 });
 
 // Post request to create a user (Registration)
-app.post('/customerinfo/register', async (req, res) => {
-    try {
-        const newUser = await User.create(req.body);
-        res.status(201).json(newUser);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Error creating user.' });
+//Login and check if user is already created
+app.post('/customerinfo/customer-infos', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401)
+      .json({ error: 'Invalid email or password.' });
     }
+    // Compare the hashed incoming password to the stored hash
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401)
+      .json({ error: 'Invalid email or password.' });
+    } else {
+       // Generate and send an authentication token if needed
+        res.json({ token });
+
+    }
+
+
+
+
+
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred.' });
+  }
 });
 
-// Login and check if user is already created
-app.post('/customerinfo/login', async (req, res) => {
-    const { email, password } = req.body;
 
-    try {
-        // Find the user by email
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(401).json({ error: 'Invalid email or password.' });
-        }
-        // Compare the hashed incoming password to the stored hash
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if (!passwordMatch) {
-            return res.status(401).json({ error: 'Invalid email or password.' });
-        } else {
-            // Here, you can generate and send an authentication token if needed.
-            // Ensure you have a proper authentication mechanism.
-            // res.json({ token });
-            res.json({ message: 'Login successful' });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred.' });
+
+//Get request to check and validate if user email is exact match in database
+app.get('/customerinfo/customer-info', async (req, res) => {
+  const { email} = req.query;
+  try {
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail ) {
+      res.json({ exists: true});
+
+    } else {
+      res.json({ exists: false });
     }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred.' });
+  }
 });
 
-// Get request to check and validate if user email exists in the database
-app.get('/customerinfo/check-email', async (req, res) => {
-    const { email } = req.query;
-    try {
-        const existingEmail = await User.findOne({ email });
-        if (existingEmail) {
-            res.json({ exists: true });
-        } else {
-            res.json({ exists: false });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred.' });
-    }
-});
 
+app.get('/customerinfo/customer-infos', async (req, res) => {
+  const { name, email } = req.query;
+  try {
+    const existingUsers = await User.find({ name, email });
+
+    if (existingUsers.length > 0) {
+      // At least one user with matching name and email found
+      // Assuming you want to return the name of the first matching user
+      const user = existingUsers[0];
+      res.json({ existingUser: true, name: user.name });
+    } else {
+      // User not found
+      res.json({ existingUser: false });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred.' });
+  }
+});
+//Post request to create a user
+app.post('/customerinfo/customer-info', async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error creating user.' });
+  }
+});
